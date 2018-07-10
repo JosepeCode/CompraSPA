5b7d410899ba6df2c2d7aa803ece4099a956b6a8
JosepeCode <josephidalgo1996@gmail.com>
2018-07-10 19:00:15 +0200

    initial commit

---
+++ src/app/Models/Compra.ts
@@ -0,0 +1,10 @@
+export class Compra{
+    private pid:number;
+    private name:String;
+    private precio:number;
+    constructor(pid, name, precio){
+        this.pid=pid;
+        this.name=name;
+        this.precio=precio;
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CompraComponentComponent } from './components/compra-component/compra-component.component';
+import { ArticulosComponentComponent } from './components/articulos-component/articulos-component.component';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompraComponentComponent,
+    ArticulosComponentComponent,
+    ArticulosComponent
   ],
   imports: [
     BrowserModule
